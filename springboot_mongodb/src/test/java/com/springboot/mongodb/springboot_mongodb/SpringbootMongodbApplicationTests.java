b28e8374618c45536ccba42914108d8116643736
DesignerBoobesh <154803938+DesignerBoobesh@users.noreply.github.com>
2024-01-06 11:11:57 +0530

    Springboot template

---
+++ springboot_mongodb/src/test/java/com/springboot/mongodb/springboot_mongodb/SpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mongodb.springboot_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
